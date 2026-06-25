355685fcb71e2890e4030d192a4ae9814563658d
“sohamadhikari2000” <sohamadhikari2000@gmail.com>
2026-06-26 00:45:42 +0200

    modified artifacts paths

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,7 +27,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('data\stud.csv')
+            df = pd.read_csv(os.path.join('data','stud.csv'))
             logging.info('Read the dataset as DataFrame')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
